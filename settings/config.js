32ed7bbbffdf574159a596150b6260dc64d9be6e
iMrlvn <mrlvndev@gmail.com>
2025-07-07 07:49:03 +0700

    update config.js

--- settings/config.js
+++ settings/config.js
@@ -27,7 +27,7 @@ module.exports = {
     Nodes: [
       { 
         name: "L1",
-        url: "lavalink.ix.tc:80",
+        url: "main.lavalink.ix.tc:80",
         auth: "youshallnotpass",
         secure: false,
       } 
